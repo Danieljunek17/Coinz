d6f85d5bece8480c5c30d453ae60ff2402f3be63
Siebe Baree <siebe.baree@outlook.com>
2021-09-03 10:59:21 +0200

    Create profile.js

---
+++ src/commands/info/profile.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+const { getMember } = require('../../tools/resolvers');
+const { progressBar, calcLvl } = require('../../tools/tools');
+
+module.exports.execute = async (client, message, args, data) => {
+    let member = args.join(" ") ? await getMember(args.join(" "), message.guild) : message.member;
+    if (!member) member = message.member;
+
+    let userData = await client.database.fetchUser(member.id, message.guildId);
+
+    const userLevel = calcLvl(userData.experience);
+
+    const newEmbed = new MessageEmbed()
+        .setAuthor(`User info of ${member.user.username}`, `${member.user.avatarURL()}`)
+        .setThumbnail(`${member.user.avatarURL({ dynamic: true })}`)
+        .setColor(member.displayColor)
+        .setFooter(client.config.footer)
+        .setTimestamp()
+        .addFields(
+            { name: 'Level Stats', value: `**Level:** ${userLevel}\n**Experience:** \`${userData.experience}/${userLevel * 100 + 100}\`\n\`${progressBar(userData.experience % 100, 100, 7)}\``, inline: false },
+            { name: 'Money', value: `** Cash:** ${data.guild.currency} ${userData.economy.cash}\n ** Bank:** ${data.guild.currency} ${userData.economy.bank}\n ** Net:** ${data.guild.currency} ${userData.economy.net}`, inline: true },
+            { name: 'Social', value: `** Followers:** ${userData.social.followers}\n ** Likes:** ${userData.social.likes}\n ** Unread Comments:** ${userData.social['comments'].length}`, inline: true }
+        );
+    await message.reply({ embeds: [newEmbed] });
+}
+
+module.exports.help = {
+    name: "profile",
+    aliases: ["user-info", "userinfo", "memberinfo", "member-info"],
+    description: "Get a lot of information about a member.",
+    usage: ["profile [member]"],
+    examples: ["profile @Siebe", "profile Discord#0000", "profile"],
+    extraFields: [],
+    image: "",
+    enabled: true,
+    category: "info",
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    ownerOnly: false,
+    cooldown: 5000
+}
